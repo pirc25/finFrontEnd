c5a37c191b05b92f8819bbb333a785e35975a1d8
Richard <rchrdbrneo@gmail.com>
2025-01-09 22:06:39 -0500

    build Project

--- config.ts
+++
@@ -1,2 +0,0 @@
-const API_URL: string = "http://192.168.1.24:8000/api/v1/";
-export default API_URL;
--- src/page/contacs.page/ContacsPage.tsx
+++
@@ -1,7 +0,0 @@
-// import CompDataGrid from "../../components/CompDataGrid/CompDataGrid";
-
-export default function ContacsPage() {
-  //   const cols: string[] = ["ID", "Codigo", "Descripcion", "Costo", "Precio"];
-  //   const url = "contacts";
-  return <div>{/* <CompDataGrid cols={cols} url={url} /> */}</div>;
-}
