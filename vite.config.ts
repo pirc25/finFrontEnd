0c14657558d3d59f580ea4cce376f320f2897292
Richard Burneo <rchrdbrneo@gmail.com>
2025-01-10 09:12:15 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/finFront/",
+  base: "/finFrontEnd/",
   plugins: [react()],
   resolve: {
     alias: {
